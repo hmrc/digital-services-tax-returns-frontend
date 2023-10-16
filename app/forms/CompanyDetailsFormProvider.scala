40a56fe75df4b9bce3c6aaeb72d7df80a9e9e7e8
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2023-10-16 18:03:40 +0100

    DDCYLS-3209:scaffold company details page

---
+++ app/forms/CompanyDetailsFormProvider.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import javax.inject.Inject
+
+import forms.mappings.Mappings
+import play.api.data.Form
+import play.api.data.Forms._
+import models.CompanyDetails
+
+class CompanyDetailsFormProvider @Inject() extends Mappings {
+
+  val companyNameMaxLength = 35
+  val uniqueTaxReferenceMaxLength =  35
+
+   def apply(): Form[CompanyDetails] = Form(
+     mapping(
+      "companyName" -> text("companyDetails.error.companyName.required")
+        .verifying(maxLength(companyNameMaxLength, "companyDetails.error.companyName.length")),
+      "uniqueTaxpayerReference" -> text("companyDetails.error.uniqueTaxpayerReference.required")
+        .verifying(maxLength(uniqueTaxReferenceMaxLength, "companyDetails.error.uniqueTaxpayerReference.length"))
+    )(CompanyDetails.apply)(CompanyDetails.unapply)
+   )
+ }
