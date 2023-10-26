fa817a41264090cb6a3ae5a3eb433322e4b11e72
wp94 <43645818+wp94@users.noreply.github.com>
2023-10-26 17:22:51 +0100

    DDCYLS-3214 scala formatting

--- test/forms/SocialMediaLossFormProviderSpec.scala
+++ test/forms/SocialMediaLossFormProviderSpec.scala
@@ -22,7 +22,7 @@ import play.api.data.FormError
 class SocialMediaLossFormProviderSpec extends BooleanFieldBehaviours {
 
   val requiredKey = "socialMediaLoss.error.required"
-  val invalidKey = "error.boolean"
+  val invalidKey  = "error.boolean"
 
   val form = new SocialMediaLossFormProvider()()
 
