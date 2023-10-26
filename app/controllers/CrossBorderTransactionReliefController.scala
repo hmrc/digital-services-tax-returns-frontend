9c4336f17a39eaee06e1b88b47e5a1d86fac71f9
wp94 <43645818+wp94@users.noreply.github.com>
2023-10-26 16:21:23 +0100

    DDCYLS-3220 WIP cross border transaction relief

--- app/controllers/CrossBorderTransactionReliefController.scala
+++ app/controllers/CrossBorderTransactionReliefController.scala
@@ -1,9 +1,26 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import controllers.actions._
 import forms.CrossBorderTransactionReliefFormProvider
+
 import javax.inject.Inject
-import models.Mode
+import models.{Mode, UserAnswers}
 import navigation.Navigator
 import pages.CrossBorderTransactionReliefPage
 import play.api.i18n.{I18nSupport, MessagesApi}
@@ -28,10 +45,10 @@ class CrossBorderTransactionReliefController @Inject()(
 
   val form = formProvider()
 
-  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData) {
     implicit request =>
 
-      val preparedForm = request.userAnswers.get(CrossBorderTransactionReliefPage) match {
+      val preparedForm = request.userAnswers.getOrElse(UserAnswers(request.userId)).get(CrossBorderTransactionReliefPage) match {
         case None => form
         case Some(value) => form.fill(value)
       }
@@ -39,7 +56,7 @@ class CrossBorderTransactionReliefController @Inject()(
       Ok(view(preparedForm, mode))
   }
 
-  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData).async {
     implicit request =>
 
       form.bindFromRequest().fold(
@@ -48,7 +65,7 @@ class CrossBorderTransactionReliefController @Inject()(
 
         value =>
           for {
-            updatedAnswers <- Future.fromTry(request.userAnswers.set(CrossBorderTransactionReliefPage, value))
+            updatedAnswers <- Future.fromTry(request.userAnswers.getOrElse(UserAnswers(request.userId)).set(CrossBorderTransactionReliefPage, value))
             _              <- sessionRepository.set(updatedAnswers)
           } yield Redirect(navigator.nextPage(CrossBorderTransactionReliefPage, mode, updatedAnswers))
       )
