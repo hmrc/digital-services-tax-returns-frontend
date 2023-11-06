01d4a7f543d9f807f8e638edc6f75584e85ee418
Tapiwa Tiyemba <142234222+tapiwa-tiyemba@users.noreply.github.com>
2023-11-06 16:11:29 +0000

    LDS ignore

--- test/forms/ReportMediaAlternativeChargeFormProviderSpec.scala
+++ test/forms/ReportMediaAlternativeChargeFormProviderSpec.scala
@@ -21,7 +21,6 @@ import play.api.data.FormError
 
 class ReportMediaAlternativeChargeFormProviderSpec extends BooleanFieldBehaviours {
 
-  // LDS ignore
   val requiredKey = "reportMediaAlternativeCharge.error.required"
   val invalidKey  = "error.boolean"
 
