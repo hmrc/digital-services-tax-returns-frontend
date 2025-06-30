6737036d0504a6713dff06738cc0bd1ed2654acb
Dips <13121210+dips7189@users.noreply.github.com>
2025-06-30 09:46:40 +0100

    DDCYLS-6959 upgrading libraries (#72)
    * DDCYLS-6959 upgrading libraries
    
    * DDCYLS-6959 formatting code

--- app/connectors/DSTConnector.scala
+++ app/connectors/DSTConnector.scala
@@ -22,38 +22,42 @@ import models.registration.{Period, Registration}
 import models.returns.Return
 import play.api.Logging
 import play.api.http.Status.INTERNAL_SERVER_ERROR
+import play.api.libs.json.Json
 import uk.gov.hmrc.http.HttpReads.Implicits._
-import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse}
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
+import java.net.URI
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
-class DSTConnector @Inject() (http: HttpClient, servicesConfig: ServicesConfig)(implicit
+class DSTConnector @Inject() (http: HttpClientV2, servicesConfig: ServicesConfig)(implicit
   executionContext: ExecutionContext
 ) extends Logging {
 
   private val backendURL: String = servicesConfig.baseUrl("digital-services-tax") + "/digital-services-tax"
 
   def lookupRegistration()(implicit hc: HeaderCarrier): Future[Option[Registration]] =
-    http.GET[Option[Registration]](s"$backendURL/registration")
+    http.get(new URI(s"$backendURL/registration").toURL).execute[Option[Registration]]
 
   def lookupOutstandingReturns()(implicit hc: HeaderCarrier): Future[Set[Period]] =
-    http.GET[List[Period]](s"$backendURL/returns/outstanding").map(_.toSet)
+    http.get(new URI(s"$backendURL/returns/outstanding").toURL).execute[List[Period]].map(_.toSet)
 
   def lookupAmendableReturns()(implicit hc: HeaderCarrier): Future[Set[Period]] =
-    http.GET[List[Period]](s"$backendURL/returns/amendable").map(_.toSet)
+    http.get(new URI(s"$backendURL/returns/amendable").toURL).execute[List[Period]].map(_.toSet)
 
   def lookupAllReturns()(implicit hc: HeaderCarrier): Future[Set[Period]] =
-    http.GET[List[Period]](s"$backendURL/returns/all").map(_.toSet)
+    http.get(new URI(s"$backendURL/returns/all").toURL).execute[List[Period]].map(_.toSet)
 
   def lookupSubmittedReturns(periodKey: PeriodKey)(implicit hc: HeaderCarrier): Future[Option[Return]] =
-    http.GET[Option[Return]](s"$backendURL/returns/${periodKey.value}")
+    http.get(new URI(s"$backendURL/returns/${periodKey.value}").toURL).execute[Option[Return]]
 
   def submitReturn(period: Period, ret: Return)(implicit hc: HeaderCarrier): Future[Int] = {
     val encodedKey = java.net.URLEncoder.encode(period.key, "UTF-8")
-    http.POST[Return, HttpResponse](s"$backendURL/returns/$encodedKey", ret) map { response =>
-      response.status
+    http.post(new URI(s"$backendURL/returns/$encodedKey").toURL).withBody(Json.toJson(ret)).execute[HttpResponse].map {
+      response =>
+        response.status
     }
   } recoverWith { case e: Exception =>
     logger.warn(s"An Exception thrown from downstream service: ${e.getMessage}")
