82721e0ac0d7a6574913d8322c9fff8591c4016f
accenture-dami <203887119+accenture-dami@users.noreply.github.com>
2026-09-09 14:43:20 +0100

    Platform Upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.11.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.13.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
@@ -21,12 +21,12 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.scalacheck"         %% "scalacheck"              % "1.19.0",
+    "org.scalacheck"         %% "scalacheck"              % "1.20.0",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.2",
     "org.scalatestplus"      %% "scalacheck-1-15"         % "3.2.11.0",
     "org.scalatestplus"      %% "mockito-3-12"            % "3.2.10.0",
     "io.github.wolfendale"   %% "scalacheck-gen-regexp"   % "1.1.0",
-    "org.jsoup"               % "jsoup"                   % "1.23.1",
+    "org.jsoup"               % "jsoup"                   % "1.23.2",
     "com.beachape"           %% "enumeratum-scalacheck"   % "1.9.8",
     "org.mockito"             % "mockito-core"            % "5.23.0",
     "io.chrisdavenport"      %% "cats-scalacheck"         % "0.3.2"
